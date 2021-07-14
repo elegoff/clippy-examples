6c8b7491a75a947050c2e6b5343c67c02d845c16
Eric Le Goff <elegoff@protonmail.com>
2021-07-14 18:25:05 +0200

    almost swapped lint

---
+++ src/almost_swapped.rs
@@ -0,0 +1,7 @@
+#[allow(unused_assignments)]
+pub fn demo(){
+    let mut a = 42;
+    let mut b = 0;
+    a = b;
+    b = a;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod absurd;
+mod almost_swapped;
 
 fn main() {
     println!("Hello, world!");
     absurd::demo();
+    almost_swapped::demo();
 }
