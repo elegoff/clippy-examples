7080677c64759eea0dacb7a7b26a78fc5ce6e505
Eric Le Goff <elegoff@protonmail.com>
2021-07-15 12:05:29 +0200

    Approx constants

---
+++ src/approx_constant.rs
@@ -0,0 +1,4 @@
+pub fn demo(){
+    let x = 3.14;
+    let y = 1_f64 / x;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod absurd;
 mod almost_swapped;
+mod approx_constant;
 
 fn main() {
     println!("Hello, world!");
     absurd::demo();
     almost_swapped::demo();
+    approx_constant::demo();
 }
