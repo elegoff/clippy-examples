f635356343f8c3c944d1ff0636c336208ba5c308
Eric Le Goff <elegoff@protonmail.com>
2021-07-15 12:16:07 +0200

    Add a file containing unit test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod absurd;
 mod almost_swapped;
 mod approx_constant;
+mod with_coverage;
 
 fn main() {
     println!("Hello, world!");
     absurd::demo();
     almost_swapped::demo();
     approx_constant::demo();
+    with_coverage::add(40, 2);
 }
