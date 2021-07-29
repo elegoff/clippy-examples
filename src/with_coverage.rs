344295ac877c11d0b900180fe02c26fdee3a6204
Eric Le Goff <elegoff@protonmail.com>
2021-07-29 17:29:37 +0200

    add more lines for coverage demo

--- src/with_coverage.rs
+++ src/with_coverage.rs
@@ -1,4 +1,47 @@
+#[allow(unused_variables)]
 pub fn add(a: i32, b: i32) -> i32 {
+
+    //Adding some useless intermediate calcultions
+    //For the sake of artificially add covered lines
+    let x = 5u32;
+
+    let y = {
+        let x_squared = x * x;
+        let x_cube = x_squared * x;
+
+        // This expression will be assigned to `y`
+        x_cube + x_squared + x
+    };
+
+    let n = 5;
+
+    if n < 0 {
+        print!("{} is negative", n);
+    } else if n > 0 {
+        print!("{} is positive", n);
+    } else {
+        print!("{} is zero", n);
+    }
+
+    let big_n =
+        if n < 10 && n > -10 {
+            println!(", and is a small number, increase ten-fold");
+
+            // This expression returns an `i32`.
+            10 * n
+        } else {
+            println!(", and is a big number, halve the number");
+
+            // This expression must return an `i32` as well.
+            n / 2
+            // TODO ^ Try suppressing this expression with a semicolon.
+        };
+    //   ^ Don't forget to put a semicolon here! All `let` bindings need it.
+
+    println!("{} -> {}", n, big_n);
+
+
+    //only this one liner is actually relevant :)
     a + b
 }
 
